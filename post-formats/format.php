b524b0cf17b79d481fb37dde28b5b69af85595ee
Anna <vintagemaniac@gmail.com>
2015-04-09 15:45:43 +0200

    Update format.php
    Conditional inclusion of comments template.

--- post-formats/format.php
+++ post-formats/format.php
@@ -67,6 +67,11 @@
 
                 </footer> <?php // end article footer ?>
 
-                <?php //comments_template(); ?>
+                <?php 
+                // If comments are open or we have at least one comment, load up the comment template
+                  if ( comments_open() || '0' != get_comments_number() ) {
+                    comments_template(); 
+                  }
+                ?>
 
               </article> <?php // end article ?>
